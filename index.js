6979525ae64f5664002e2da17ba3a6cb5d9e57c1
黄笑 <huangxiao221@jd.com>
2021-01-14 20:57:25 +0800

    晚上功能

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const fs = require("fs");
 const os = require("os");
-const prompts = require('prompts');
+const inquirer = require("inquirer");
 const path = require("path");
 const home = os.homedir();
 //用这种方式以免不同平台斜线不一样
-const fullFolderName = path.join(home, 'todo');
-prompts.override(require('yargs').argv);
+const fullFolderName = path.join(home, "todo");
 
 module.exports.add = (content) => {
   // a+如果不存在就创建
@@ -15,8 +14,8 @@ module.exports.add = (content) => {
     } else {
       let list;
       try {
-       list = JSON.parse(data.toString())  
-       console.log(list,'list')
+        list = JSON.parse(data.toString());
+        console.log(list, "list");
       } catch (err) {
         list = [];
       }
@@ -28,54 +27,99 @@ module.exports.add = (content) => {
       fs.writeFile(fullFolderName, JSON.stringify(list), { flag: "w+" }, (err, fd) => {
         if (err) console.log(err);
       });
-      (async () => {
-        const response = await prompts([
+      inquirer
+        .prompt([
           {
-            type: 'select',
-            name: 'title',
-            message: 'Pick task',
-            choices: list.map((item,index)=>{
-               return {title:`${index}-${item.done?'done':'x'}_${item.title}`, value:index}
-            })
+            type: "list",
+            name: "index",
+            message: "choose task",
+            choices: [
+              ...[{ name: "quit", value: -1 }],
+              ...list.map((item, index) => {
+                return { name: `${index} ${item.done ? "[done]" : "[x]"}_${item.title}`, value: index };
+              }),
+              ...[{ name: "创建", value: -2 }],
+            ],
           },
-          {
-            type: 'confirm',
-            name: 'value',
-            message: 'Have you done?',
-            initial: true
+        ])
+        .then((selectedItem) => {
+          console.log(JSON.stringify(selectedItem));
+          if (selectedItem.index == -2) {
+            console.log("去创建吧骚年");
+            return;
           }
-        ]);
-        // 找出第几个   
-        list[response.title].done = response.value
-        fs.writeFile(fullFolderName, JSON.stringify(list),{ flag: "w+" },(err)=>{
-            console.log(err)
-        })
-        console.log(list);
-      })();
+          if (selectedItem.index == -1) {
+            console.log("去退出吧骚年");
+            return;
+          }
+          inquirer.prompt([
+            {
+              type: "list",
+              name: "action",
+              message: "have you done?",
+              choices: [
+                {name:'退出',value:'quit'},
+                {name:'完成',value:'done'},
+                
+              ],
+            },
+          ]).then((answer)=>{
+            console.log(JSON.stringify(answer))
+            list[selectedItem.index].done = answer.action
+            fs.writeFile(fullFolderName, JSON.stringify(list), { flag: "w+" }, (err, fd) => {
+                if (err) console.log(err);
+              });
+             
+          })
+        });
+
+      //   (async () => {
+      //     const response = await prompts([
+      //       {
+      //         type: 'select',
+      //         name: 'title',
+      //         message: 'Pick task',
+      //         choices: list.map((item,index)=>{
+      //            return {title:`${index}-${item.done?'done':'x'}_${item.title}`, value:index}
+      //         })
+      //       },
+      //       {
+      //         type: 'confirm',
+      //         name: 'value',
+      //         message: 'Have you done?',
+      //         initial: true
+      //       }
+      //     ]);
+      //     // 找出第几个
+      //     list[response.title].done = response.value
+      //     fs.writeFile(fullFolderName, JSON.stringify(list),{ flag: "w+" },(err)=>{
+      //         console.log(err)
+      //     })
+      //     console.log(list);
+      //   })();
     }
   });
 };
 
+module.exports.clear = () => {
+  fs.writeFile(fullFolderName, [], { flag: "w+" }, (err, fd) => {
+    if (err) console.log(err);
+  });
+};
 
-module.exports.clear =()=>{
-    fs.writeFile(fullFolderName,[], { flag: "w+" },(err,fd)=>{
-        if (err) console.log(err);
-    })
-}
-
-module.exports.done=()=>{
-    fs.readFile(fullFolderName,{ flag: "a+" },(err, data) =>{
-        if(err){
-            console.log(err,'errdone')
-        }
-        console.log(data.toString(),'data')
-    })
-}
-module.exports.showAll=()=>{
-    fs.readFile(fullFolderName,{ flag: "a+" },(err, data) =>{
-        if(err){
-            console.log(err,'errdone')
-        }
-        console.log(data.toString())
-    })
-}
+module.exports.done = () => {
+  fs.readFile(fullFolderName, { flag: "a+" }, (err, data) => {
+    if (err) {
+      console.log(err, "errdone");
+    }
+    console.log(data.toString(), "data");
+  });
+};
+module.exports.showAll = () => {
+  fs.readFile(fullFolderName, { flag: "a+" }, (err, data) => {
+    if (err) {
+      console.log(err, "errdone");
+    }
+    console.log(data.toString());
+  });
+};
